718d9e747e30ce768716592e942ec4f47f959572
Koczabamba <50304669+Koczabamba@users.noreply.github.com>
2019-05-06 19:42:13 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/flyingbathtub/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.flyingbathtub;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button Jeden = findViewById(R.id.button);
+        Jeden.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((Button)findViewById(R.id.button)).setBackgroundColor(Color.parseColor("#ffffff"));
+            }
+        });
+
+    }
+}
