d83e393e1148d0ef329621883619a8cbfadcf2af
Koczabamba <50304669+Koczabamba@users.noreply.github.com>
2019-05-22 22:14:07 +0200

    Add files via upload

--- app/src/main/java/com/example/flyingbathtub/MainActivity.java
+++ app/src/main/java/com/example/flyingbathtub/MainActivity.java
@@ -10,9 +10,9 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
 
-    private void alojzy(String aleksandra){
+    private void wyslij_znak(String znak) {
 //        aleksandra="2";
-        ((TextView)findViewById(R.id.calcScreen)).setText(aleksandra);
+        ((TextView) findViewById(R.id.calcScreen)).setText(((TextView)findViewById(R.id.calcScreen)).getText() + znak);
 
     }
 
@@ -27,8 +27,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 ((Button)findViewById(R.id.buttonMultiplication)).setBackgroundColor(Color.parseColor("#ffffff"));
-//                ((TextView)findViewById(R.id.textView)).setText("orangutan");
-                alojzy("*");
+                wyslij_znak("*");
             }
         });
         Button Plus= findViewById(R.id.buttonPlus);
@@ -36,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 ((Button)findViewById(R.id.buttonPlus)).setBackgroundColor(Color.parseColor("#ffffff"));
-                ((TextView)findViewById(R.id.calcScreen)).setText("+");
+                wyslij_znak("+");
             }
         });
         Button Division= findViewById(R.id.buttonDivision);
@@ -44,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 ((Button)findViewById(R.id.buttonDivision)).setBackgroundColor(Color.parseColor("#ffffff"));
-                ((TextView)findViewById(R.id.calcScreen)).setText(":");
+                wyslij_znak(":");
             }
         });
         Button Minus= findViewById(R.id.buttonMinus);
@@ -52,84 +51,84 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 ((Button)findViewById(R.id.buttonMinus)).setBackgroundColor(Color.parseColor("#ffffff"));
-                ((TextView)findViewById(R.id.calcScreen)).setText("-");
+                wyslij_znak("-");
             }
         });
         Button Jeden= findViewById(R.id.button1);
         Jeden.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                ((TextView)findViewById(R.id.calcScreen)).setText("1");
+                wyslij_znak("1");
             }
         });
         Button Dwa= findViewById(R.id.button2);
         Dwa.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                ((TextView)findViewById(R.id.calcScreen)).setText("2");
+                wyslij_znak("2");
             }
         });
         Button Trzy= findViewById(R.id.button3);
         Trzy.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                ((TextView)findViewById(R.id.calcScreen)).setText("3");
+                wyslij_znak("3");
             }
         });
         Button Cztery= findViewById(R.id.button4);
         Cztery.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                ((TextView)findViewById(R.id.calcScreen)).setText("4");
+                wyslij_znak("4");
             }
         });
         Button Piec= findViewById(R.id.button5);
         Piec.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                ((TextView)findViewById(R.id.calcScreen)).setText("5");
+                wyslij_znak("5");
             }
         });
         Button Szesc= findViewById(R.id.button6);
         Szesc.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                ((TextView)findViewById(R.id.calcScreen)).setText("6");
+                wyslij_znak("6");
             }
         });
         Button Siedem= findViewById(R.id.button7);
         Siedem.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                ((TextView)findViewById(R.id.calcScreen)).setText("7");
+                wyslij_znak("7");
             }
         });
         Button Osiem= findViewById(R.id.button8);
         Osiem.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                ((TextView)findViewById(R.id.calcScreen)).setText("8");
+                wyslij_znak("8");
             }
         });
         Button Dziewiec= findViewById(R.id.button9);
         Dziewiec.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                ((TextView)findViewById(R.id.calcScreen)).setText("9");
+                wyslij_znak("9");
             }
         });
         Button Zero= findViewById(R.id.button0);
         Zero.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                ((TextView)findViewById(R.id.calcScreen)).setText("0");
+                wyslij_znak("0");
             }
         });
         Button Przecinek= findViewById(R.id.buttonPoint);
         Przecinek.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                ((TextView)findViewById(R.id.calcScreen)).setText(",");
+                wyslij_znak(",");
             }
         });
         Button Rownosc= findViewById(R.id.buttonEqual);
